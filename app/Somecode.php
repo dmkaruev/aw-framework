dc0b0c1cfbf1d317cbcf5a2820ea7b84891a1239
Dmitry Karuev <dmkaruev@mail.ru>
2025-07-28 14:45:03 +0300

    Request класс

---
+++ app/Somecode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class Somecode
+{
+    public function test()
+    {
+        echo 'Some text';
+    }
+}
+
+?>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Somecode\Framework\Http\Request;
+
+$request = Request::createFromGlobals();
+
+dd($request);
+
+?>
